aba80306587f63a8d80275feaa1724047b5c8ae1
racoursey-cloud <racoursey@gmail.com>
2026-03-31 19:58:29 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        surface: '#16181c',
+        border:  '#25282e',
+        hover:   '#1c1e23',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
